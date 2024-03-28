b9f0851ed18ff4650781446d0def069f726d98a6
Oksana Kolisnyk <zazera777@gmail.com>
2024-03-28 09:55:51 +0200

    Added logic of switching the colour between 2 WorkType states

--- src/app/experience-info/experience-info.component.ts
+++ src/app/experience-info/experience-info.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { WorkExperience } from '../models/work-experience';
+import { WorkType } from '../models/worktype-enum';
 
 @Component({
   selector: 'app-experience-info',
@@ -7,6 +8,15 @@ import { WorkExperience } from '../models/work-experience';
   styleUrls: ['./experience-info.component.css']
 })
 export class ExperienceInfoComponent implements OnInit {
+  isColourWorkTypeChanged(): string {
+    // return this.experienceInfo.workType === WorkType.FullTime;
+    if (this.experienceInfo.workType  === WorkType.FullTime) {
+      return 'fullTime-mode';
+    } else {
+      return 'internship-mode'
+    }
+  }
+
   @Input()
   experienceInfo!: WorkExperience;
 
